d46474d00bf129354be1858b2eb6a313a5b26ee0
Babavaraprasad <bapi@itu.dk>
2022-08-22 12:46:48 +0200

    added routing for meetup page

--- todo2/src/components/meetups/MeetupItem.js
+++ todo2/src/components/meetups/MeetupItem.js
@@ -1,21 +1,21 @@
-import classes from './MeetupItem.module.css';
+import classes from "./MeetupItem.module.css";
 
-function MeetupItem(props)
-{
-return(
-<li>
-<div>
-    <img src={props.image} alt={props.title}> </img>
-</div>
-<div>
-<h3>{props.title}</h3>
-<address>{props.adress}</address>
-<p>{props.description}</p>
-</div>
-<div>
-    <button>to favourite</button>
-</div>
-</li>
-);
+function MeetupItem(props) {
+  return (
+    <li className={classes.item}>
+      <div className={classes.image}>
+        <img src={props.image} alt={props.title} />
+      </div>
+      <div className={classes.content}>
+        <h3>{props.title}</h3>
+        <address>{props.address}</address>
+        <p>{props.description}</p>
+      </div>
+      <div className={classes.actions}>
+        <button>to favourite</button>
+      </div>
+      <br />
+    </li>
+  );
 }
 export default MeetupItem;
--- todo2/src/pages/AllMeetup.js
+++ todo2/src/pages/AllMeetup.js
@@ -1,39 +1,32 @@
-const dummydata=[
-    {
-        id: 'm1',
-        title: 'This is a first meetup',
-        image:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Meetupstreet 5, 12345 Meetup City',
-        description:
-          'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-      },
-      {
-        id: 'm2',
-        title: 'This is a second meetup',
-        image:
-          'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-        address: 'Meetupstreet 5, 12345 Meetup City',
-        description:
-          'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-      },
+import MeetupList from "../components/meetups/MeetupList";
+
+const dummydata = [
+  {
+    id: "m1",
+    title: "This is a first meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description:
+      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+  },
+  {
+    id: "m2",
+    title: "This is a second meetup",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+    address: "Meetupstreet 5, 12345 Meetup City",
+    description:
+      "This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!",
+  },
 ];
 
-
-
-function AllMeetup()
-{
-    return (
-       <section>
-        <h1>AllMeetup</h1>
-<ul>
-        {dummydata.map((meetup)=>{
-            return <li key={meetup.title}>{meetup.title}</li>
-
- })
-        }
-       </ul>
-        </section>
-    );
-} 
+function AllMeetup() {
+  return (
+    <section>
+      <h1>AllMeetup</h1>
+     <MeetupList meetups={dummydata}/>
+    </section>
+  );
+}
 export default AllMeetup;
